626ccd6bdc35850ab84e1eadb6f2f9ae798f8791
João Victor <victor.dev1301@gmail.com>
2023-04-15 13:28:31 -0300

    Refatorado

--- js/script.js
+++ js/script.js
@@ -1,28 +1,17 @@
-/*Script do botão menu*/
-const menu = document.querySelector('#menu');
-menu.addEventListener('click', () => {
-  const menuDiv = document.querySelector('.header-container nav ul');
+function exibirMenu() {
+  const menuDiv = document.querySelector('#listaDoMenu');
   menuDiv.style.display = menuDiv.style.display == 'none' ? 'flex' : 'none';
-})
+}
 
-/*Script de mudança de tema*/
-/*Note que a mudança de tema é feita com adição e remoção da classe
-tema escuro, que está definida no css*/
-const tema = document.querySelector('#tema');
-let contador = 0;
-
-tema.addEventListener('click', () => {
+function mudarTema() {
   const html = document.querySelector('html');
-  if (contador === 0) {
-    html.classList.add('tema-claro');
-    contador = 1;
-  } else {
-    html.classList.remove('tema-claro');
-    contador = 0;
-  }
-});
+  html.classList.contains('tema-claro') ? html.classList.remove('tema-claro') : html.classList.add('tema-claro');
+}
 
-
+document.addEventListener('click', e => {
+  if (e.target.id === 'menu') exibirMenu();
+  if (e.target.id === 'tema') mudarTema();
+})
 
 
 
